5af151c3211b50908c6048898985f8cc6c47ad64
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-09 18:18:20 +0000

    Implemented api.js to make HTTP request

---
+++ src/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const searchImages = async () => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    header: {
+      Authorization: "Client-ID 2DJbsLCoM3ZsCxg9ovMne4L3GpLHkOonA8QuKjd-OW8",
+    },
+    params: {
+      query: "cars",
+    },
+  });
+
+  console.log(response)
+  return response;
+};
+
+export default searchImages;

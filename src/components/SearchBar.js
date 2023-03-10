ce8cd744d4057af010620e0a03c0cdecfeefb470
yushaushaibu <yushaushaibu1@gmail.com>
2023-03-10 13:23:26 +0000

    searchImage function receive term from SearchBar component and makes API request with the term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,7 +5,7 @@ function SearchBar({ onSubmit }) {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    onSubmit("cars");
+    onSubmit(term);
   };
 
   const handleChange = (event) => {
